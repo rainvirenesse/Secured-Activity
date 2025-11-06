4881c2c8d3443c23e9d78813537adbad0ac7bd55
Rain <rainvirenesse@gmail.com>
2025-11-06 17:13:05 +0800

    Initial commit: Symfony project with protected dashboard route

---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+class DashboardController extends AbstractController
+{
+    #[Route('/dashboard', name: 'app_dashboard')]
+    #[IsGranted('ROLE_USER')]
+    public function index(): Response
+    {
+        return new Response("Welcome to the dashboard! (Protected Area)");
+    }
+}
